9737239d0e343b8529e37cafc2844aa29fe70101
sshspcc <sshspcc@gmail.com>
2019-11-18 17:59:47 +0900

    sakurastamp

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rally
-for stamp rally
+# スタンプラリーのプロトタイプ
+## 使い方
+- 部活の追加
+clubList.jsonに部活のidと名前を入力します．  
+jsonファイルの書式については適宜調べてください．  
+（idは予測できないようにランダムな文字列にするといいと思います．  
+[こちら](http://www.japan9.com/cgi/rand_num.cgi)のサイトでランダムな文字列が生成できるので試してみてください．）  
+- QRコードの作成
+QRコード作成サイトで，url?id=(生成したid)をQRコード化します．  
+例)https://himantyu-yuma.github.io/stamprally/?id=6kmzbf  
+### ※注意：スタンプ画像のファイル名をidにするのを忘れないようにしてください．
+
+以上．
---
+++ main.js
@@ -0,0 +1,89 @@
+let start = function () {
+    getClub();
+}
+
+
+let getClub = function () {
+    clubDic = {};
+    $.ajax({
+        url: 'clubList.json',
+        type: 'GET',
+        dataType: 'json'
+    })
+        .done(function (data) {
+            // 部活のidと名前のjsonを連想配列に
+            clubDic = data;
+            // 部活のid
+            keys = Object.keys(clubDic);
+            // json取得後にDOM構築
+            makeDOM();
+            getId();
+            checkProgress();
+            preload();
+        })
+        .fail(function (data) {
+            alert('読み込みに失敗しました')
+            console.log(data);
+        });
+}
+
+let preload = function () {
+    for (let i = 0; i < keys.length; i++) {
+        $(`<img src="img/${clubDic[keys[i]]}.png"`);
+        console.log(`preload${keys[i]}`);
+    }
+}
+
+let makeDOM = function () {
+    let rowNum = 3;
+    let currentRowNum = 1;
+
+    $('#sheet').append(
+        row = $('<tr>')
+    );
+    for (let i = 0; i < keys.length; i++) {
+        if (currentRowNum > rowNum) {
+            currentRowNum = 1;
+            $('#sheet').append(
+                row = $('<tr>')
+            );
+        }
+        currentRowNum++;
+        row.append($('<td>')
+            .append($('<img>', {
+                'src': 'img/none.png',
+                'id': keys[i]
+            })
+            ));
+
+    }
+}
+
+let getId = function () {
+    if (location.search == '') {
+        return;
+    } else {
+        // クエリからid部分を取得
+        id = location.search.replace('?id=', '');
+        // もうすでに取得済みなら何もしない
+        if (localStorage.getItem(id)) {
+            return;
+        } else {
+            // localStorageに進捗を保存
+            localStorage.setItem(id, clubDic[id]);
+        }
+    }
+    $(`#${id}`).attr('src', 'img/stamp.png')
+    alert(`${clubDic[id]}のスタンプを獲得しました！`)
+}
+
+let checkProgress = function () {
+
+    for (let i = 0; i < keys.length; i++) {
+        if (localStorage.getItem(keys[i])) {
+            $(`#${keys[i]}`).attr('src', `img/${keys[i]}.png`);
+        }
+    }
+}
+
+start();
